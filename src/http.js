43601c49839e299efd90304084414e4524b58645
Melih Berkay Aydin <berkaydin34@gmail.com>
2024-04-09 14:38:29 +0200

    started to add fetching and updating classe

--- src/http.js
+++ src/http.js
@@ -8,3 +8,14 @@ export async function fetchAvailableClasses() {
 
   return resData.places;
 }
+
+export async function fetchUserClasses() {
+  const response = await fetch("http://localhost:3000/user-places");
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch user places");
+  }
+
+  return resData.places;
+}
--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -1,32 +1,86 @@
 import ClassSection from "../components/Class Section/ClassSection";
 import styles from "./Classes.module.css";
-import { fetchAvailableClasses } from "../http";
+import { fetchAvailableClasses, fetchUserClasses } from "../http";
 import { useState, useEffect } from "react";
 
 export default function Classes() {
-  const [isFetching, setIsFetching] = useState(false);
+  const selectedPlace = useRef();
+  const [isFetchingAllClasses, setIsFetchingAllClasses] = useState(false);
+  const [isFetchingUserClasses, setIsFetchingUserClasses] = useState(false);
   const [availableClasses, setavailableClasses] = useState([]);
+  const [userClasses, setUserClasses] = useState([]);
   const [error, setError] = useState();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   useEffect(() => {
     async function fetchPlaces() {
-      setIsFetching(true);
+      setIsFetchingAllClasses(true);
 
       try {
         const places = await fetchAvailableClasses();
         setavailableClasses(places);
-        setIsFetching(false);
+        setIsFetchingAllClasses(false);
       } catch (error) {
         setError({
           message:
-            error.message || "Could not fetch places, please try again later.",
+            error.message || "Could not fetch classes, please try again later.",
         });
-        setIsFetching(false);
+        setIsFetchingAllClasses(false);
       }
     }
 
     fetchPlaces();
   }, []);
+
+  useEffect(() => {
+    async function fetchClasses() {
+      setIsFetchingUserClasses(true);
+      try {
+        const userClasses = await fetchUserClasses();
+        setUserClasses(userClasses);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch user classes." });
+      }
+
+      setIsFetchingUserClasses(false);
+    }
+
+    fetchClasses();
+  }, []);
+
+  async function handleSelectPlace(selectedClass) {
+    // await updateUserPlaces([selectedPlace, ...userPlaces]);
+
+    setUserClasses((prevPickedClasses) => {
+      if (!prevPickedClasses) {
+        prevPickedClasses = [];
+      }
+      if (
+        prevPickedClasses.some((lecture) => lecture.id === selectedClass.id)
+      ) {
+        return prevPickedClasses;
+      }
+      return [selectedClass, ...prevPickedClasses];
+    });
+
+    try {
+      await updateUserPlaces([selectedClass, ...userClasses]);
+    } catch (error) {
+      setUserClasses(userPlaces);
+      setErrorUpdatingPlaces({
+        message: error.message || "Failed to update places.",
+      });
+    }
+  }
+
+  function handleStartRemovePlace(place) {
+    setModalIsOpen(true);
+    selectedPlace.current = place;
+  }
+
+  function handleStopRemovePlace() {
+    setModalIsOpen(false);
+  }
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;
@@ -34,12 +88,26 @@ export default function Classes() {
 
   return (
     <section className={styles.placesCategory}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+      <ClassSection
+        title="User Classes"
+        classes={availableClasses}
+        fallbackText="No class is found"
+        onSelectPlace={console.log}
+        isLoading={isFetchingUserClasses}
+        loadingText="Classes are loading"
+      />
       <ClassSection
         title="All Classes"
         classes={availableClasses}
         fallbackText="No class is found"
         onSelectPlace={console.log}
-        isLoading={isFetching}
+        isLoading={isFetchingAllClasses}
         loadingText="Classes are loading"
       />
     </section>
